2d4e6fd86fe61aa3e5938b6adee18e7b70d1b0ec
werlang e feix <104462193+werlangfeix@users.noreply.github.com>
2024-03-07 14:59:32 -0300

    Add files via upload

---
+++ programa-ao2024meusobjetivos-main/programa-ao2024meusobjetivos-main/joao-feix-programaçao2024/main.js
@@ -0,0 +1,22 @@
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
+
+for(let i=0;i <botoes.length;i++){
+    botoes[i].onclick = function(){
+
+        
+        for(let j=0;j<botoes.length;j++){
+            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
+        }
+
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
+    }
+    console.log(i);
+}
+const contadores =document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
+let TempoAtual = new Date();
+
+contadores[0].textContent = tempoObjetivo1 - TempoAtual;
